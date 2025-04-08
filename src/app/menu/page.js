2e778f43ee5fe7d221474fd9d59d11cd25059ee7
karinapecherna <karina.pecherna.ik.2024@lpnu.ua>
2025-04-08 16:58:33 +0300

    Add menu

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,14 +1,15 @@
-import Sidebar from "@/components/Sidebar"; // шлях до твого компонента
+import Sidebar from "@/components/sidebar/Sidebar"; 
+import MenuTable from "@/components/menu/Menu"; 
 
 export default function Home() {
   return (
     <main>
-       <div className="sidebar">
-                <Sidebar /> </div>
-                <div className="mainContent">
-      <h1 style={{ textAlign: "center", marginTop: "2rem" }}>
-        Меню
-      </h1>
+      <div className="sidebar">
+        <Sidebar />
+      </div>
+      <div className="mainContent">
+        <h1 style={{ textAlign: "center", marginTop: "2rem" }}>Меню</h1>
+        <MenuTable />
       </div>
     </main>
   );
